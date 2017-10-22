e9d9c80fd87ddd063516520d85f64112435aea6e
Daniel Johannsson <d12@ru.is>
2017-10-22 23:50:47 +0000

    Added some lighting to Level1 and added a main menu scene

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuScript : MonoBehaviour {
+    public GameObject mainPanel;
+    public GameObject saveGamePanel;
+    public GameObject optionsPanel;
+
+    // Universal
+
+    public void OnPressBackToMain()
+    {
+        if (!mainPanel.activeInHierarchy)
+            mainPanel.SetActive(true);
+        if (saveGamePanel.activeInHierarchy)
+            saveGamePanel.SetActive(false);
+        if (optionsPanel.activeInHierarchy)
+            optionsPanel.SetActive(false);
+    }
+
+    // Main panel
+
+    public void OnPressStart()
+    {
+        if (!saveGamePanel.activeInHierarchy)
+            saveGamePanel.SetActive(true);
+        mainPanel.SetActive(false);
+    }
+
+    public void OnPressOptions()
+    {
+        if (!optionsPanel.activeInHierarchy)
+            optionsPanel.SetActive(true);
+        mainPanel.SetActive(false);
+    }
+
+    public void OnPressQuit()
+    {
+        Application.Quit();
+    }
+
+    // Save game panel
+
+    public void OnPressSave1()
+    {
+
+    }
+
+    public void OnPressSave2()
+    {
+
+    }
+
+    public void OnPressSave3()
+    {
+
+    }
+}
